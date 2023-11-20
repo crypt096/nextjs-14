a523ff8f9cbc6cd598829c8308035dce33a634b0
Alex <31933549+crypt096@users.noreply.github.com>
2023-11-20 20:24:16 +0100

    Create nested routes.

---
+++ hello-world/src/app/blog/first/page.tsx
@@ -0,0 +1,5 @@
+export default function BlogFirst() {
+  return (
+    <div>BlogFirst</div>
+  )
+}
---
+++ hello-world/src/app/blog/second/page.tsx
@@ -0,0 +1,5 @@
+export default function BlogSecond() {
+  return (
+    <div>BlogSecond</div>
+  )
+}
