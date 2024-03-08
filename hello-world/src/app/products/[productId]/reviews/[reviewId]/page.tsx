f86f5df0e474822ba23a56aaa50ff6d9debb1ec9
Alex <31933549+crypt096@users.noreply.github.com>
2024-03-08 18:19:42 +0100

    Update page.tsx

--- hello-world/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ hello-world/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,14 +1,25 @@
 import { notFound } from "next/navigation"
 
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 export default function ReviewDetail({ params }: {
   params: {
     productId: string,
     reviewId: string
   }
 }) {
+  const random = getRandomInt(2);
+
+  if (random === 1) {
+    throw new Error("Error loading");
+  }
+
   if (parseInt(params.reviewId) > 1000) {
     notFound();
   }
+
   return (
     <div>Review {params.reviewId} for product {params.productId}</div>
   )
